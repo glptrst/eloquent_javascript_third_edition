514bf51bdcc845503dc19778d3f4556d5d06b536
gp <giulio.pietroiusti@gmail.com>
2018-11-14 00:34:01 +0100

    A list.

--- 04_data.js
+++ 04_data.js
@@ -65,8 +65,8 @@ function range(start, end, step) {
 //
 // Thinking back to the notes about side effects and pure functions in
 // the previous chapter, which variant do you expect to be useful in
-// more situations? Which one runs faster
-    ?
+// more situations? Which one runs faster?
+    
 function reverseArray(arr) {
     let result = [];
     for (let i = arr.length-1; i >= 0; i--) {
@@ -87,7 +87,7 @@ function reverseArrayInPlace(arr) {
 // structures. A common data structure is the list (not to be confused with
 // array). A list is a nested set of objects, with the first object holding a
 // reference to the second, the second to the third, and so on.
-
+//
 // let list = {
 //   value: 1,
 //   rest: {
@@ -109,7 +109,7 @@ function reverseArrayInPlace(arr) {
 // rest: list} (with list referring to the binding defined earlier), they are
 // both independent lists, but they share the structure that makes up their last
 // three elements. The original list is also still a valid three-element list.
-
+//
 // Write a function arrayToList that builds up a list structure like the one
 // shown when given [1, 2, 3] as argument. Also write a listToArray function
 // that produces an array from a list. Then add a helper function prepend, which
@@ -117,17 +117,42 @@ function reverseArrayInPlace(arr) {
 // the front of the input list, and nth, which takes a list and a number and
 // returns the element at the given position in the list (with zero referring to
 // the first element) or undefined when there is no such element.
-
+//
 // If you haven’t already, also write a recursive version of nth
 
 function arrayToList(array) {
-    let current = {value: array[array.length-1],
-		   rest: null};
+    let current = {
+	value: array[array.length-1],
+	rest: null
+    };
     for (let i = array.length-2; i >= 0; i--) {
-	let next = { value: array[i],
-		     rest: current};
+	let next = {
+	    value: array[i],
+	    rest: current
+	};
 	current = next;
     }
     return current;
 }
 
+function listToArray(list) {
+    let result = [];
+    while (1) {
+	result.push(list.value);
+	if (list.rest === null) break;
+	else list = list.rest;
+    }
+    return result;
+}
+
+function prepend(element, list) {
+    return {
+	value: element,
+	rest: list
+    };
+}
+
+function nth(list, number) {
+    if (number === 0) return list.value;
+    else return nth(list.rest, number - 1);
+}
