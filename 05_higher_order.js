66c1bbb2f0d968050e5c0fae580877460f3f7341
gp <giulio.pietroiusti@gmail.com>
2018-12-01 22:48:52 +0100

    Dominant writing direction.

--- 05_higher_order.js
+++ 05_higher_order.js
@@ -65,3 +65,62 @@ console.log(every([2, 4, 16], n => n < 10));
 // → false
 console.log(every([], n => n < 10));
 // → true
+
+// DOMINANT WRITING DIRECTION
+//
+// Write a function that computes the dominant writing direction in a string of
+// text. Remember that each script object has a direction property that can be
+// "ltr" (left to right), "rtl" (right to left), or "ttb" (top to bottom).
+//
+// The dominant direction is the direction of a majority of the characters that
+// have a script associated with them. The characterScript and countBy functions
+// defined earlier in the chapter are probably useful here.
+
+function dominantDirection(text){
+    let chars = [];
+    for (let char of text) {
+	chars.push(char);
+    }
+
+    let directions = chars.map(char => char.codePointAt(0))
+	.map(code => characterScript(code)).filter(script => script !== null)
+	.map(script => script.direction);
+    
+    return countBy(directions, el => el).reduce( (a, b) => a.count > b.count ? a : b).name;
+};
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+// console.log(characterScript(121));
+// → {name: "Latin", …}
+
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+// console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+// → [{name: false, count: 2}, {name: true, count: 3}]
