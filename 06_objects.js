97b9576baa4b40beb5ec43b572cc6a684af9b969
gp <giulio.pietroiusti@gmail.com>
2018-12-17 16:22:06 +0100

    GroupIterator

--- 06_objects.js
+++ 06_objects.js
@@ -85,3 +85,42 @@ class Group {
 // group.delete(10);
 // console.log(group.has(10));
 // // → false
+
+
+// Iterable groups
+//
+// Make the Group class from the previous exercise iterable. Refer to the
+// section about the iterator interface earlier in the chapter if you aren’t
+// clear on the exact form of the interface anymore.
+//
+// If you used an array to represent the group’s members, don’t just return the
+// iterator created by calling the Symbol.iterator method on the array. That
+// would work, but it defeats the purpose of this exercise.
+//
+// It is okay if your iterator behaves strangely when the group is modified
+// during iteration.
+
+class GroupIterator {
+    constructor(group) {
+	this.members = group.members;
+	this.index = 0;
+	this.current = this.members[this.index] || null;
+    }
+    next() {
+	if (this.index === this.members.length)
+	    return {done: true};
+	this.current = this.members[this.index++];
+	return {value: this.current, done: false};
+    }
+}
+
+Group.prototype[Symbol.iterator] = function() {
+    return new GroupIterator(this);
+};
+
+// for (let value of Group.from(["a", "b", "c"])) {
+//   console.log(value);
+// }
+// // → a
+// // → b
+// // → c
