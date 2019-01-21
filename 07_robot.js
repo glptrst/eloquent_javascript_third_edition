f4f74ef241df8590a29c8cb9f391e633b6aa2150
gp <giulio.pietroiusti@gmail.com>
2019-01-21 15:53:31 +0100

    Use reduce method instead of loop.

--- 07_robot.js
+++ 07_robot.js
@@ -35,8 +35,10 @@ function countRobotTurns(state, robot, memory) {
     memory = action.memory;
   }
 }
-
-// runRobotAnimation(VillageState.random(), yourRobot, memory);
+// Notice that, even if goalOrientedrobot is better than routeRobot when we
+// consider village states with 5 parcels, when we consider village states with
+// more parcels (as I do in the loop) routeRobot ends up being better. The
+// author does not mention this.
 
 
 // ROBOT EFFICIENCY
@@ -49,44 +51,33 @@ function countRobotTurns(state, robot, memory) {
 // function to verify whether you improved the robot.
 
 function yourRobot({place, parcels}, route) {
+    // compute best parcel to deliver
+    let bestToDeliver = parcels.filter(p => p.place === place).map(function(p) {
+	return {parcel: p, distance: findRoute(roadGraph, place, p.address).length};
+    }).reduce( function(a, b ) { if (a.distance <= b.distance) return a; else return b; }, {isEmpty: true} );
+    // compute best parcel to pick up
+    let bestToPickup  = parcels.filter(p => p.place !== place).map(function(p) {
+	return {parcel: p, distance: findRoute(roadGraph, place, p.place).length};
+    }).reduce( function(a, b) { if (a.distance <= b.distance) return a; else return b; }, {isEmpty: true} );
+
     if (route.length == 0) { // if we have no route planned, compute route
-	// parcels to deliver
-	let toDeliver = parcels.filter(p => p.place === place).map(function(p) {
-	    return {parcel: p, distance: findRoute(roadGraph, place, p.address).length};
-	});
-	// compute best parcel to deliver
-	let bestToDeliver = toDeliver[0];
-	for (let i = 1; i < toDeliver.length; i++)
-	    if (toDeliver[i].distance < bestToDeliver.distance)
-		bestToDeliver = toDeliver[i];
-	// parcels to pick up
-	let toPickup  = parcels.filter(p => p.place !== place).map(function(p) {
-	    return {parcel: p, distance: findRoute(roadGraph, place, p.place).length};
-	});
-	// compute best parcel to pick up
-	let bestToPickup = toPickup[0];
-	for (let i = 1; i < toPickup.length; i++)
-	    if (toPickup[i].distance < bestToPickup.distance)
-		bestToPickup = toPickup[i];
-	
-	if (bestToPickup === undefined) { // if there are no parcel to pick
-	    parcel = bestToDeliver.parcel;
+	if (bestToPickup.isEmpty) { // if there are no parcel to pick up
+	    route = findRoute(roadGraph, place, bestToDeliver.parcel.address);
 	} else {
-	    if (bestToDeliver === undefined) { // if there are no parcel to deliver
-		parcel = bestToPickup.parcel;
+	    if (bestToDeliver.isEmpty) { // if there are no parcel to deliver
+		route = findRoute(roadGraph, place, bestToPickup.parcel.place);
 	    } else { // if there are both parcels to pick up and parcels to deliver
-		if (bestToPickup.distance === bestToDeliver.distance) {
-		    parcel = bestToPickup.parcel;
-		} else {
+		if (bestToPickup.distance === bestToDeliver.distance) { // if there's no difference in distance, go for the one to pick up
+		    route = findRoute(roadGraph, place, bestToPickup.parcel.place);
+		} else { // otherwise choose the closer place
 		    parcel = bestToPickup.distance < bestToDeliver.distance ? bestToPickup.parcel : bestToDeliver.parcel;
+		    if (bestToPickup.distance < bestToDeliver.distance) {
+			route = findRoute(roadGraph, place, bestToPickup.parcel.place);
+		    } else {
+			route = findRoute(roadGraph, place, bestToDeliver.parcel.address);
+		    }
 		}
 	    }
-	}
-	
-	if (parcel.place != place) { // if that parcel is not where the robot currently is
-	    route = findRoute(roadGraph, place, parcel.place); // then compute the route to pick it up
-	} else { // if that parcel is where the roboto currently is
-	    route = findRoute(roadGraph, place, parcel.address); // then compute the route to deliver it
 	}
     }
     return {direction: route[0], memory: route.slice(1)};
