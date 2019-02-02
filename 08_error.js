67f42276d97e057d1fbbb88044d9f1fc31a96613
gp <giulio.pietroiusti@gmail.com>
2019-02-03 00:01:48 +0100

    Locked box.

--- 08_error.js
+++ 08_error.js
@@ -32,3 +32,38 @@ function reliableMultiply(a, b) {
 
 console.log(reliableMultiply(8, 8));
 // → 64
+
+
+
+// THE LOCKED BOX
+//
+// Consider the following (rather contrived) object:
+//
+// const box = {
+//   locked: true,
+//   unlock() { this.locked = false; },
+//   lock() { this.locked = true;  },
+//   _content: [],
+//   get content() {
+//     if (this.locked) throw new Error("Locked!");
+//     return this._content;
+//   }
+// };
+//
+// It is a box with a lock. There is an array in the box, but you can get at it
+// only when the box is unlocked. Directly accessing the private _content
+// property is forbidden.
+//
+// Write a function called withBoxUnlocked that takes a function value as
+// argument, unlocks the box, runs the function, and then ensures that the box
+// is locked again before returning, regardless of whether the argument function
+// returned normally or threw an exception.
+
+function withBoxUnlocked(body) {
+    if (box.locked) box.unlock();
+    try {
+	return body();
+    } finally {
+	box.lock();
+    }
+}
