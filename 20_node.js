eb476031c90da4af51792b253493a0c9f66cdc56
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2019-05-14 14:47:31 +0200

    Promises search tool.

--- 20_node.js
+++ 20_node.js
@@ -40,3 +40,35 @@ function search(regexp, arr) {
 	}  
     }
 }
+
+// promises version
+const {readFile, stat, readdir} = require("fs").promises;
+
+let regexp = new RegExp(process.argv[2], 'g');
+let paths = process.argv.slice(3);
+
+search(regexp, paths);
+
+// Take regexp and array of paths
+async function search(regexp, arr) {
+    for (let i = 0; i < arr.length; i++) {
+	let path = arr[i];
+	try {
+	    let stats = await stat(path);
+	    if (stats.isDirectory()) {
+		let dirContent = await readdir(path);
+
+		dirContent = dirContent.map((el) => {
+		    return path + '/' + el;
+		});;
+
+		search(regexp, dirContent);
+	    } else {
+		let content = await readFile(path, 'utf8');
+		console.log(path + ': ' + regexp.test(content));
+	    }
+	} catch (e) {
+	    console.log(e);
+	}
+    }  
+}
