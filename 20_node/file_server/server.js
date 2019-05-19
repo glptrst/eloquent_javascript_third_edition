5d4a48098a613a60cab9966d6fedfa3987be9f5d
glptrst <giulio.pietroiusti@gmail.com>
2019-05-19 23:32:37 +0200

    Serve html page which includes a js file.

--- 20_node/file_server/server.js
+++ 20_node/file_server/server.js
@@ -95,15 +95,15 @@ methods.PUT = async function(request) {
 const {mkdir} = require("fs").promises;
 
 methods.MKCOL = async function(request) {
-    let path = urlPath(request.url);
-    let stats;
-    try {
-	stats = await stat(path);
-    } catch (error) {
-	if (error.code != "ENOENT") throw error;
-	await mkdir(path);
-	return {status: 204};
-    }
-    if (stats.isDirectory()) return {status: 204};
-    else return {status: 400, body: "Not a directory"};
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+    await mkdir(path);
+    return {status: 204};
+  }
+  if (stats.isDirectory()) return {status: 204};
+  else return {status: 400, body: "Not a directory"};
 };
