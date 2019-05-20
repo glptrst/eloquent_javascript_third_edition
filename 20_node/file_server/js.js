2d8c45db33a0015ef3f4e05526431e81c4346bb8
glptrst <giulio.pietroiusti@gmail.com>
2019-05-21 00:42:21 +0200

    Show content of page in textarea.

--- 20_node/file_server/js.js
+++ 20_node/file_server/js.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+
+let textarea = document.querySelector('textarea');
+// show content of page.html into textarea
+fetch("page.html").then(response => {
+    response.text()
+	.then(content => {
+	    textarea.value = content;
+	});
+});
