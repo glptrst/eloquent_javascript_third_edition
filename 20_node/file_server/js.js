dd3998c846222ac5f90f332e5fe37b2eb6647ad3
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2019-05-23 15:00:25 +0200

    Show content of pages in textarea.

--- 20_node/file_server/js.js
+++ 20_node/file_server/js.js
@@ -1,18 +1,44 @@
+let select = document.querySelector('select');
 let textarea = document.querySelector('textarea');
-// show content of page.html into textarea
-fetch("page.html").then(response => {
+
+showContent(textarea.value);
+
+// show options in the select menu
+fetch('/').then(response => {
     response.text()
 	.then(content => {
-	    textarea.value = content;
+	    let files = content.split('\n');
+	    for (let file of files) {
+		let option = document.createElement('option');
+		let optionText = document.createTextNode(file);
+		option.appendChild(optionText);
+		select.appendChild(option);
+	    }
 	});
 });
 
-let form = document.querySelector("form");
-form.addEventListener("submit", event => {
-    event.preventDefault();
-    fetch("page.html", {method: "PUT", body: form.text.value}).then(resp => {
-	console.log(resp.status);
-    });
+// Change content of textarea with changing option in the select menu
+select.addEventListener('change', event => {
+    showContent(event.target.value);
 });
 
+select.addEventListener('change', event => {
+    //console.log(event.target.value);
+    fetch(event.target.value)
+	.then(content => {
+	    content.text().then(text => {
+		textarea.value = text;
+	    });
+	});
+});
+
+// Get name of page. Get its content and show it in the textarea
+function showContent (page) {
+    fetch(page)
+	.then(content => {
+	    content.text().then(text => {
+		textarea.value = text;
+	    });
+	});
+}
 
---
+++ 20_node/file_server/page.js
@@ -0,0 +1,18 @@
+let textarea = document.querySelector('textarea');
+// show content of page.html into textarea
+fetch("page.html").then(response => {
+    response.text()
+	.then(content => {
+	    textarea.value = content;
+	});
+});
+
+let form = document.querySelector("form");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    fetch("page.html", {method: "PUT", body: form.text.value}).then(resp => {
+	console.log(resp.status);
+    });
+});
+
+
